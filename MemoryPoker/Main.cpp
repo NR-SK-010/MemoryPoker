bba6f59f15d0e2cc59777c4567f5cf8a76551b7e
SikasiKasi <SikasiKasi029@gmail.com>
2025-02-20 16:18:12 +0900

    アセット準備

--- MemoryPoker/Main.cpp
+++ MemoryPoker/Main.cpp
@@ -9,6 +9,26 @@ void Main()
 	Window::Resize(800, 600);
 	Scene::Resize(1600, 1200);
 
+	//アセット登録
+	FontAsset::Register(U"Title", 70, Typeface::Regular);
+	FontAsset::Register(U"Text", 30);
+
+	AudioAsset::Register(U"BGM", U"Sounds/Night_View.mp3");
+	AudioAsset::Register(U"button", U"Sounds/button.mp3");
+	AudioAsset::Register(U"cancel", U"Sounds/cancel.mp3");
+	AudioAsset::Register(U"flip", U"Sounds/cardflip.mp3");
+	AudioAsset::Register(U"coin", U"Sounds/coin.mp3");
+
+	//アセット事前ロード
+	FontAsset::Load(U"Title");
+	FontAsset::Load(U"Text");
+
+	AudioAsset::Load(U"BGM");
+	AudioAsset::Load(U"button");
+	AudioAsset::Load(U"cancel");
+	AudioAsset::Load(U"flip");
+	AudioAsset::Load(U"coin");
+
 	//シーン管理用
 	App manager;
 
