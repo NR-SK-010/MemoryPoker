95b1ea2bbc1fb31e59ead1c8e40a3484a8255891
SikasiKasi <SikasiKasi029@gmail.com>
2025-02-20 14:59:36 +0900

    Common.hpp(共有データ,シーン管理用)追加

---
+++ MemoryPoker/Common.hpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+//シーン名
+enum class State
+{
+	Title,
+	Config,
+	Rule,
+	Memory,
+	Select,
+	Bet,
+	ShowDown,
+	Result
+};
+
+//共有データ
+struct GameData
+{
+
+};
+
+using App = SceneManager<State, GameData>;
