41976e30461aa7ba1531acc4490ca79a2ee6d90b
SikasiKasi <SikasiKasi029@gmail.com>
2025-02-20 15:14:53 +0900

    ウィンドウ,シーンサイズ設定

--- MemoryPoker/Main.cpp
+++ MemoryPoker/Main.cpp
@@ -1,18 +1,23 @@
-﻿ # include <Siv3D.hpp> // Siv3D v0.6.14
+﻿#include <Siv3D.hpp> // Siv3D v0.6.14
+#include "Common.hpp"
 
 void Main()
 {
-<<<<<<< HEAD
+	//実ウィンドウ,シーンサイズ設定
+	Scene::SetResizeMode(ResizeMode::Keep);
+	Window::SetStyle(WindowStyle::Sizable);
+	Window::Resize(800, 600);
+	Scene::Resize(1600, 1200);
 
-=======
->>>>>>> 70f2d4f8b9dd0f7e223055ec34c9f6761586e62d
+	//シーン管理用
+	App manager;
+
 
 	while (System::Update())
 	{
-
+		if (not manager.update())
+		{
+			break;
+		}
 	}
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 70f2d4f8b9dd0f7e223055ec34c9f6761586e62d
