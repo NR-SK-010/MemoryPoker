7d4aecdc462a46c9b7ce67033e3581e8ac916a39
SikasiKasi <SikasiKasi029@gmail.com>
2025-02-27 23:43:03 +0900

    SelectCardシーンの図形描画

--- MemoryPoker/SelectCard.cpp
+++ MemoryPoker/SelectCard.cpp
@@ -16,4 +16,34 @@ void SelectCard::draw() const
 {
 	//背景色
 	Scene::SetBackground(Palette::Green);
+
+	//メニューボタン
+	Button(MenuButton, FontAsset(U"Button"), U"メニュー", Palette::Black);
+
+	//Player手札表示
+	PlayerHandsArea.draw(Palette::White);
+	PlayerHandsArea.drawFrame(2, 2, Palette::Black);
+	for (int i = 0; i < getData().player.getHands().size(); i++)
+	{
+		const Vec2 center{ 575 + i * 90, 1050 };
+		getData().pack(getData().cards[i]).drawAt(center);
+	}
+
+	//選択したカードの表示
+	RoleNameArea.draw(Palette::White);
+	RoleNameArea.drawFrame(2, 2, Palette::Black);
+	SelectCardArea.draw(Palette::White);
+	SelectCardArea.drawFrame(2, 2, Palette::Black);
+
+	//CPU手札表示
+	CpuHandsArea.draw(Palette::White);
+	CpuHandsArea.drawFrame(2, 2, Palette::Black);
+	for (int i = 0; i < getData().cpu.getHands().size(); i++)
+	{
+		const Vec2 center{ 575 + i * 90, 220 };
+		getData().pack(getData().cards[i]).drawAt(center);
+	}
+
+	Button(RecommendButton, FontAsset(U"Button"), U"おすすめ", Palette::Black);
+	Button(OkButton, FontAsset(U"Button"), U"OK", Palette::Black, getData().player.getSelectCards().size() == 3);
 }
--- MemoryPoker/SelectCard.hpp
+++ MemoryPoker/SelectCard.hpp
@@ -13,5 +13,11 @@ public:
 	void draw() const override;
 
 private:
-
+	Rect MenuButton{ Arg::center(1480, 70), 200, 80 }; //メニューボタン
+	RoundRect PlayerHandsArea{ Arg::center(800, 1050), 600, 200, 7 }; //Player手札表示エリア
+	RoundRect CpuHandsArea{ Arg::center(800, 200), 600, 250, 7 }; //CPU手札表示エリア
+	RoundRect RoleNameArea{ 450, 550, 700, 100, 7 }; //役の名前表示エリア
+	RoundRect SelectCardArea{ 500, 660, 600, 280 , 7 }; //選択したカードの表示エリア
+	Rect RecommendButton{ 1200, 900, 300, 100 }; //「おすすめ」ボタン(カードの自動選択)
+	Rect OkButton{ 1200, 1050, 300, 100 }; //「OK」ボタン。これを押すとBetシーンへ遷移
 };
