16ef8822fbcef138fcb8014efb5139c3c88da807
SikasiKasi <SikasiKasi029@gmail.com>
2025-03-01 17:27:21 +0900

    ShowDownシーン描画

--- MemoryPoker/ShowDown.cpp
+++ MemoryPoker/ShowDown.cpp
@@ -30,4 +30,69 @@ void ShowDown::draw() const
 	Scene::SetBackground(Palette::Green);
 
 	Button(MenuButton, FontAsset(U"Button"), U"メニュー", Palette::Black);
+
+	//Player選択カード表示
+	PlayerSelectCardArea.draw(Palette::White);
+	PlayerSelectCardArea.drawFrame(2, 2, Palette::Black);
+	for (int i = 0; i < getData().player.getSelectCards().size(); i++)
+	{
+		const Vec2 center{ 710 + i * 90, 950 };
+		getData().pack(getData().cards[getData().player.getSelectCards()[i]]).drawAt(center);
+	}
+
+	//PlayerName表示
+	PlayerNameArea.draw(Palette::White);
+	PlayerNameArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(getData().player.getName()).drawAt(1250, 865, Palette::Black);
+
+	//PlayerChip表示
+	PlayerChipArea.draw(Palette::White);
+	PlayerChipArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(U"CHIP").drawAt(1250, 1100, Palette::Black);
+	FontAsset(U"Text")(getData().player.getChip()).drawAt(1450, 1100, Palette::Black);
+
+	//PlayerBet額表示
+	PlayerBetArea.draw(Palette::White);
+	PlayerBetArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(U"BET").drawAt(1250, 970, Palette::Black);
+	FontAsset(U"Text")(getData().player.getInitChip() - getData().player.getChip()).drawAt(1450, 970, Palette::Black);
+
+	//PlayerRoleText表示
+	PlayerRoleArea.draw(Palette::White);
+	PlayerRoleArea.drawFrame(2, 2, Palette::Black);
+
+
+	//CPU選択カード表示
+	CpuSelectCardArea.draw(Palette::White);
+	CpuSelectCardArea.drawFrame(2, 2, Palette::Black);
+	for (int i = 0; i < getData().cpu.getSelectCards().size(); i++)
+	{
+		const Vec2 center{ 710 + i * 90, 250 };
+
+		//表向きの場合は裏返しておく
+		if (getData().cards[getData().cpu.getSelectCards()[i]].isFaceSide) getData().cards[getData().cpu.getSelectCards()[i]].flip();
+
+		getData().pack(getData().cards[getData().cpu.getSelectCards()[i]]).drawAt(center);
+	}
+
+	//CpuName表示
+	CpuNameArea.draw(Palette::White);
+	CpuNameArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(getData().cpu.getName()).drawAt(150, 90, Palette::Black);
+
+	//CPUChip表示
+	CpuChipArea.draw(Palette::White);
+	CpuChipArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(U"CHIP").drawAt(150, 315, Palette::Black);
+	FontAsset(U"Text")(getData().cpu.getChip()).drawAt(350, 315, Palette::Black);
+
+	//CpuBet額表示
+	CpuBetArea.draw(Palette::White);
+	CpuBetArea.drawFrame(2, 2, Palette::Black);
+	FontAsset(U"Text")(U"BET").drawAt(150, 195, Palette::Black);
+	FontAsset(U"Text")(getData().cpu.getInitChip() - getData().cpu.getChip()).drawAt(350, 195, Palette::Black);
+
+	//CpuRoletext表示
+	CpuRoleArea.draw(Palette::White);
+	CpuRoleArea.drawFrame(2, 2, Palette::Black);
 }
